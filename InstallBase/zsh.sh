f3cdb7b138f10fa5def312b052cfcf5ba274e0fb
Iñigo Marin <inigomaringarcia@gmail.com>
2020-06-05 22:07:17 +0200

    oh-my-zsh add to correct error

--- InstallBase/zsh.sh
+++ InstallBase/zsh.sh
@@ -2,7 +2,7 @@
 
 sudo apt install zsh -y
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
