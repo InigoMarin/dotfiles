3db1da0e6d49c304eeadf052fbbbe00b65893dff
Iñigo Marin <inigomaringarcia@gmail.com>
2020-06-05 22:46:48 +0200

    Zsh best plugins

--- InstallBase/zsh.sh
+++ InstallBase/zsh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo apt install zsh -y
+sudo apt install zsh-syntax-highlighting -y
+sudo apt install zsh-autosuggestions -y
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
