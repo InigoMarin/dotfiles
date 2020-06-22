2e9820625e569934c2bcb3fb5a596317f4810d71
Iñigo Marin <inigomaringarcia@gmail.com>
2020-06-22 20:06:04 +0200

    Transmission-daemon stop when install terminate

--- InstallBase/transmission.sh
+++ InstallBase/transmission.sh
@@ -10,5 +10,6 @@ git clone https://github.com/tremc/tremc.git tremc
 cd tremc && sudo make && cd .. && rm -fr tremc
 
 sudo systemctl disable transmission-daemon
+sudo systemctl stop transmission-daemon
 
 cd ~/Descargas && mkdir transmission && cd transmission && mkdir Downloads && mkdir Incompletes
