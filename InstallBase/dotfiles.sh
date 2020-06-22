c7056941754e7dc95d404d7856db502fc56a1448
Iñigo Marin <inigomaringarcia@gmail.com>
2020-06-22 19:59:36 +0200

    Repository dotfiles error

--- InstallBase/dotfiles.sh
+++ InstallBase/dotfiles.sh
@@ -1,6 +1,6 @@
 echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"' >> $HOME/.zshrc
 source $HOME/.zshrc
 echo ".dotfiles" >> .gitignore
-git clone --bare https://www.github.com/InigoMarin/repo.git $HOME/.dotfiles
+git clone --bare https://www.github.com/InigoMarin/dotfiles.git $HOME/.dotfiles
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
