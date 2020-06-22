0bb6762a7b6525ade5d25c31de5f60360342d467
Iñigo Marin <inigomaringarcia@gmail.com>
2020-06-22 20:31:29 +0200

    dotfiles correct

--- InstallBase/dotfiles.sh
+++ InstallBase/dotfiles.sh
@@ -1,6 +1,8 @@
 echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"' >> $HOME/.zshrc
-source $HOME/.zshrc
 echo ".dotfiles" >> .gitignore
 git clone --bare https://www.github.com/InigoMarin/dotfiles.git $HOME/.dotfiles
-dotfiles checkout
-dotfiles config --local status.showUntrackedFiles no
+rm $HOME/.zshrc
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+
+echo 'Restart shell and user dotfiles alias'
