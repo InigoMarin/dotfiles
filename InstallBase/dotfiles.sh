7c4414054b84bb1d381b0db8e096d6dcb59834d1
Iñigo Marin <inigomaringarcia@gmail.com>
2020-06-23 12:53:26 +0200

    Correct dotfiles.sh

--- InstallBase/dotfiles.sh
+++ InstallBase/dotfiles.sh
@@ -1,7 +1,11 @@
 echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"' >> $HOME/.zshrc
 echo ".dotfiles" >> .gitignore
 git clone --bare https://www.github.com/InigoMarin/dotfiles.git $HOME/.dotfiles
-rm $HOME/.zshrc
+
+echo "Backup .zshrc and .profile"
+mv  $HOME/.zshrc $HOME/.zshrc_backup
+mv $HOME/.profile $HOMe/.profile_backup
+
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
